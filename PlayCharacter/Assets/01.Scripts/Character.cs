714a138572fe114107f1d86843a5229da67784c2
ruddnjsvos12 <ruddnjsvos12@naver.com>
2019-02-23 13:31:43 +0900

    2월 23일

--- PlayCharacter/Assets/01.Scripts/Character.cs
+++ PlayCharacter/Assets/01.Scripts/Character.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Character : MonoBehaviour
 {
+    
     // 캐릭터컨트롤러를 _animator에 세팅 
-    [SerializeField] private Animator _animator;
+    
+    [SerializeField] AnimationController _animationController;
+
     // Start is called before the first frame update
     void Start()
     {
-        IdleState();
+        //IdleState();
 
+        /*        
+         _animationController.AddEndEvent(()=>
+        {
+            Debug.Log("Animation Test");
+            // Idle -> Wait
+            // Wait -> Kick
+            // Kick -> Idle
+            
+        });*/
+        ChangeState(eState.IDLE);
     }
 
     // Update is called once per frame
@@ -22,11 +36,46 @@ public class Character : MonoBehaviour
     //상태
     void IdleState()
     {
-        string triggerName = "idle1";
+        //_animator.SetTrigger("idle1");
+
+        /*
         int randValue = Random.Range(1, 6);
         triggerName = "idle" + randValue;
         _animator.SetTrigger(triggerName);
-
+        */
     }
 
+    void WaitState()
+    {
+        //_animator.SetTrigger("idle2");
+        
+    }
+
+    void KickState()
+    {
+        //_animator.SetTrigger("idle4");   
+    }
+    enum eState
+    {
+        IDLE,
+        WAIT,
+        KICK,
+    }
+
+    void ChangeState(eState state)
+    {
+        switch (state)
+        {
+            case eState.IDLE:
+                IdleState();
+                break;
+            case eState.WAIT:
+                WaitState();
+                break;
+
+            case eState.KICK:
+                KickState();
+                break;
+        }
+    }
 }
